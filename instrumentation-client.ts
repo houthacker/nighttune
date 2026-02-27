3a00b309e6e9fd87f01d1c7548bcb27e967942e0
houthacker <houthacker@pm.me>
2026-02-27 15:31:28 +0100

    Enable compression of log exportw

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,11 +1,13 @@
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http'
+import { CompressionAlgorithm } from '@opentelemetry/otlp-exporter-base'
 import { BatchLogRecordProcessor, LoggerProvider } from '@opentelemetry/sdk-logs'
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
 import { resourceFromAttributes } from '@opentelemetry/resources'
 import { logs } from '@opentelemetry/api-logs'
 
 const logExporter = new OTLPLogExporter({
-    url: '/log-proxy'
+    url: '/log-proxy',
+    compression: CompressionAlgorithm.GZIP
 })
 const logRecordProcessor = new BatchLogRecordProcessor(logExporter, {
     maxExportBatchSize: 10,
