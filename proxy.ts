7e3ca462ca18316ed9ebf161dad7e1a64768f350
houthacker <houthacker@pm.me>
2026-04-14 14:12:12 +0200

    Make distributed tracing service optional.

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,16 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
+import { OptionalService } from './src/utils/constants'
+import { isServiceEnabled } from './src/utils/optionalServices'
 
 export async function proxy(request: NextRequest) {
-    if (request.nextUrl.pathname === '/log-proxy') {
-        const rewriteUrl = new URL(process.env.NEXT_PUBLIC_OTEL_EXPORTER_OTLP_LOGS_ENDPOINT!)
-        return NextResponse.rewrite(rewriteUrl)
+
+    // Do not proxy /log-proxy if distributed tracing has been disabled
+    if (isServiceEnabled(OptionalService.DistributedTracing)) {
+        if (request.nextUrl.pathname === '/log-proxy') {
+            const rewriteUrl = new URL(process.env.NEXT_PUBLIC_OTEL_EXPORTER_OTLP_LOGS_ENDPOINT!)
+            return NextResponse.rewrite(rewriteUrl)
+        }
     }
 
     return NextResponse.next()
